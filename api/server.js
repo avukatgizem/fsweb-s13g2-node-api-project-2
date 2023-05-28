018a6bdf1a7c36f1b24682932e60e0309e4dd7ea
avukatgizem <125642428+avukatgizem@users.noreply.github.com>
2023-05-28 13:43:20 +0300

    .

--- api/server.js
+++ api/server.js
@@ -1,3 +1,9 @@
 // server için gerekli olanları burada ayarlayın
+const express=require("express");
+const server=express();
+const postRouter=require("./posts/posts.router");
+server.use (express.json());
 
+server.use("/api/posts");
 // posts router'ını buraya require edin ve bağlayın
+module.exports = server;
